09338f896ad1b25f4082b682c84ea4fb950201d7
scorpneo <scorpneo@gmail.com>
2022-01-27 14:30:17 -0600

    Potential trajectory for 2 element pickup and drop off in MeepMeep

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -2,10 +2,14 @@ package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryVelocityConstraint;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.SampleMecanumDrive;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+import org.jetbrains.annotations.NotNull;
 
 public class MeepMeepTesting {
     public static void main(String args[]) {
@@ -22,7 +26,7 @@ public class MeepMeepTesting {
          Pose2d blue_1_shipping_hub_pos = new Pose2d(-21.5,  45.75, Math.toRadians(-70));
          Pose2d red1_shipping_hub_pos      = new Pose2d(-21.5, -45.75, Math.toRadians(70));
 
-         Pose2d blue_2_shipping_hub_pos     = new Pose2d(-8.625,  45.75, Math.toRadians(-90));
+         Pose2d blue_2_shipping_hub_pos     = new Pose2d(-10.625,  45.75, Math.toRadians(-90));
          Pose2d red2_shipping_hub_pos      = new Pose2d(-1.625, -45.75, Math.toRadians(110));
 
          Pose2d blue_duck_wheel_pos       = new Pose2d(-59.75, 55, Math.toRadians(0));
@@ -31,7 +35,7 @@ public class MeepMeepTesting {
          Pose2d blue_warehouse_enter_pos  = new Pose2d( 11, 64.75, Math.toRadians(0));
          Pose2d red_warehouse_enter_pos   = new Pose2d( 11, -64.75, Math.toRadians(0));
 
-         Pose2d blue_warehouse_pos        = new Pose2d( 36.75, 64.75, Math.toRadians(0 ));
+         Pose2d blue_warehouse_pos        = new Pose2d( 38.75, 64.75, Math.toRadians(0 ));
          Pose2d red_warehouse_pos         = new Pose2d( 36.75, -64.75, Math.toRadians(0));
 
          Pose2d blue_storage_pos          = new Pose2d( -59.75, 35.5, Math.toRadians(0));
@@ -40,26 +44,41 @@ public class MeepMeepTesting {
          Pose2d blue_park_pos             = new Pose2d( 40, 36, Math.toRadians(45));
          Pose2d red_park_pos              = new Pose2d( 64, -36, Math.toRadians(-90));
 
-
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(mm)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(40, 40, Math.toRadians(180), Math.toRadians(180), 11.8)
+                .setConstraints(100, 100, Math.toRadians(180), Math.toRadians(180), 11.8)
                 .followTrajectorySequence(drive ->
                         //red duck
                         drive.trajectorySequenceBuilder(blue_2_pose_estimate)
                                 //target pos
-                                .splineToLinearHeading(blue_2_shipping_hub_pos, Math.toRadians(-90))
-                                .splineToLinearHeading(blue_warehouse_enter_pos, Math.toRadians(0))
+//                                .splineToLinearHeading(blue_2_shipping_hub_pos, Math.toRadians(-90))
+//                                .splineToLinearHeading(blue_warehouse_enter_pos, Math.toRadians(0))
+//                                //go to warehouse entrance
+//                                .splineToLinearHeading(blue_warehouse_pos, Math.toRadians(0), SampleMecanumDrive.getVelocityConstraint(15, 35, 11.8), SampleMecanumDrive.getAccelerationConstraint(40))
+//                                .splineToLinearHeading(blue_warehouse_enter_pos, Math.toRadians(0))
+//                                .splineToLinearHeading(blue_2_shipping_hub_pos, Math.toRadians(-90))
+//                                .splineToLinearHeading(blue_warehouse_enter_pos, Math.toRadians(0))
+//                                .splineToLinearHeading(blue_warehouse_pos, Math.toRadians(0), SampleMecanumDrive.getVelocityConstraint(15, 35, 11.8), SampleMecanumDrive.getAccelerationConstraint(40))
+//                                .splineToLinearHeading(blue_warehouse_enter_pos, Math.toRadians(0))
+//                                .splineToLinearHeading(blue_2_shipping_hub_pos, Math.toRadians(-90))
+//                                .splineToLinearHeading(blue_warehouse_enter_pos, Math.toRadians(0))
+//                                .splineToLinearHeading(blue_warehouse_pos, Math.toRadians(0), SampleMecanumDrive.getVelocityConstraint(15, 35, 11.8), SampleMecanumDrive.getAccelerationConstraint(40))
+
+                                .lineToLinearHeading(blue_2_shipping_hub_pos)
+                                .waitSeconds(2)
+                                .lineToLinearHeading(blue_warehouse_enter_pos)
                                 //go to warehouse entrance
-                                .splineToLinearHeading(blue_warehouse_pos, Math.toRadians(0))
-                                .splineToLinearHeading(blue_warehouse_enter_pos, Math.toRadians(0))
-                                .splineToLinearHeading(blue_2_shipping_hub_pos, Math.toRadians(-90))
-                                .splineToLinearHeading(blue_warehouse_enter_pos, Math.toRadians(0))
-                                .splineToLinearHeading(blue_warehouse_pos, Math.toRadians(0))
-                                .splineToLinearHeading(blue_warehouse_enter_pos, Math.toRadians(0))
-                                .splineToLinearHeading(blue_2_shipping_hub_pos, Math.toRadians(-90))
-                                .splineToLinearHeading(blue_warehouse_enter_pos, Math.toRadians(0))
-                                .splineToLinearHeading(blue_warehouse_pos, Math.toRadians(0))
+                                .lineToLinearHeading(blue_warehouse_pos, SampleMecanumDrive.getVelocityConstraint(15, 35, 11.8), SampleMecanumDrive.getAccelerationConstraint(40))
+                                .lineToLinearHeading(blue_warehouse_enter_pos)
+                                .lineToLinearHeading(blue_2_shipping_hub_pos)
+                                .waitSeconds(2)
+                                .lineToLinearHeading(blue_warehouse_enter_pos)
+                                .lineToLinearHeading(blue_warehouse_pos, SampleMecanumDrive.getVelocityConstraint(15, 35, 11.8), SampleMecanumDrive.getAccelerationConstraint(40))
+                                .lineToLinearHeading(blue_warehouse_enter_pos)
+                                .lineToLinearHeading(blue_2_shipping_hub_pos)
+                                .waitSeconds(2)
+                                .lineToLinearHeading(blue_warehouse_enter_pos)
+                                .splineToLinearHeading(blue_warehouse_pos, Math.toRadians(0), SampleMecanumDrive.getVelocityConstraint(15, 35, 11.8), SampleMecanumDrive.getAccelerationConstraint(40))
                                 .splineToSplineHeading(blue_park_pos, Math.toRadians(90))
                                 //enter warehouse
                                 .build()
