44d26d59fed5781c7d749c740ac1d3e607c288d2
scorpneo <scorpneo@gmail.com>
2022-01-27 13:27:45 -0600

    Potential trajectory for 2 element pickup and drop off in MeepMeep

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -9,7 +9,9 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
     public static void main(String args[]) {
-        MeepMeep mm = new MeepMeep(1000);
+        MeepMeep mm = new MeepMeep(1000)
+                .setBackgroundAlpha(1f)
+                .setBackground(MeepMeep.Background.FIELD_FREIGHTFRENZY_OFFICIAL);
 
          Pose2d blue_1_pose_estimate      = new Pose2d(-33.75, 62.625, Math.toRadians(-90));
          Pose2d red_1_pose_estimate       = new Pose2d(-37, -62.625, Math.toRadians(90));
@@ -20,7 +22,7 @@ public class MeepMeepTesting {
          Pose2d blue_1_shipping_hub_pos = new Pose2d(-21.5,  45.75, Math.toRadians(-70));
          Pose2d red1_shipping_hub_pos      = new Pose2d(-21.5, -45.75, Math.toRadians(70));
 
-         Pose2d blue_2_shipping_hub_pos     = new Pose2d(-1.625,  45.75, Math.toRadians(-110));
+         Pose2d blue_2_shipping_hub_pos     = new Pose2d(-8.625,  45.75, Math.toRadians(-90));
          Pose2d red2_shipping_hub_pos      = new Pose2d(-1.625, -45.75, Math.toRadians(110));
 
          Pose2d blue_duck_wheel_pos       = new Pose2d(-59.75, 55, Math.toRadians(0));
@@ -35,7 +37,7 @@ public class MeepMeepTesting {
          Pose2d blue_storage_pos          = new Pose2d( -59.75, 35.5, Math.toRadians(0));
          Pose2d red_storage_pos          = new Pose2d( -59.75, -35.5, Math.toRadians(0));
 
-         Pose2d blue_park_pos             = new Pose2d( 64, 36, Math.toRadians(90));
+         Pose2d blue_park_pos             = new Pose2d( 40, 36, Math.toRadians(45));
          Pose2d red_park_pos              = new Pose2d( 64, -36, Math.toRadians(-90));
 
 
@@ -46,11 +48,19 @@ public class MeepMeepTesting {
                         //red duck
                         drive.trajectorySequenceBuilder(blue_2_pose_estimate)
                                 //target pos
-                                .lineToLinearHeading(blue_2_shipping_hub_pos)
-                                .lineToLinearHeading(blue_warehouse_enter_pos)
+                                .splineToLinearHeading(blue_2_shipping_hub_pos, Math.toRadians(-90))
+                                .splineToLinearHeading(blue_warehouse_enter_pos, Math.toRadians(0))
                                 //go to warehouse entrance
-                                .lineToLinearHeading(blue_warehouse_pos)
-                                .lineToLinearHeading(blue_park_pos)
+                                .splineToLinearHeading(blue_warehouse_pos, Math.toRadians(0))
+                                .splineToLinearHeading(blue_warehouse_enter_pos, Math.toRadians(0))
+                                .splineToLinearHeading(blue_2_shipping_hub_pos, Math.toRadians(-90))
+                                .splineToLinearHeading(blue_warehouse_enter_pos, Math.toRadians(0))
+                                .splineToLinearHeading(blue_warehouse_pos, Math.toRadians(0))
+                                .splineToLinearHeading(blue_warehouse_enter_pos, Math.toRadians(0))
+                                .splineToLinearHeading(blue_2_shipping_hub_pos, Math.toRadians(-90))
+                                .splineToLinearHeading(blue_warehouse_enter_pos, Math.toRadians(0))
+                                .splineToLinearHeading(blue_warehouse_pos, Math.toRadians(0))
+                                .splineToSplineHeading(blue_park_pos, Math.toRadians(90))
                                 //enter warehouse
                                 .build()
                 );
